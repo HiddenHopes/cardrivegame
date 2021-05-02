c5260a83bff40e4d3a38da1504a1e2e6dc6bce7d
hiddenhopes <shafi.2013@gmail.com>
2021-05-02 10:41:14 +0600

    Car moving left right on key pressed

---
+++ src/Car.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class Car {
+    Image carImage;
+    int x;
+
+    Car(){
+        ImageIcon imageIcon = new ImageIcon("src/images/car.png");
+        carImage = imageIcon.getImage();
+        x = 300;
+    }
+
+    public void move(KeyEvent e){
+        int key = e.getKeyCode();
+
+       if(key == 39) x = x + 5;
+       if(key == 37) x = x - 5;
+    }
+}
